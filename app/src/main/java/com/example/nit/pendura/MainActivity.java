1a17867f09bc4307d458fe615492c4d7124d97a3
Albert Paixão <apsnet8@gmail.com>
2018-08-17 14:56:46 -0300

    starting project

---
+++ app/src/main/java/com/example/nit/pendura/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.nit.pendura;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bt_mudar_texto;
+    private EditText ed_nome, ed_apelido, ed_telefone;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Vinculando o java a interface xml
+        ed_nome = (EditText) findViewById(R.id.ed_nome);
+        ed_apelido = (EditText) findViewById(R.id.ed_apelido);
+        ed_telefone = (EditText) findViewById(R.id.ed_telefone);
+
+        bt_mudar_texto = (Button) findViewById(R.id.bt_mudar_texto);
+
+
+
+        //Evento usado para usar quando clicar no botão
+        bt_mudar_texto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do something when the corky2 is clicked } });
+                Log.i("erro", "clicou no botão mudar_texto");
+                String msg = "Clicou no botão";
+                String nome = ed_nome.getText().toString();
+                String apelido = ed_apelido.getText().toString();
+                String telefone = ed_telefone.getText().toString();
+                Toast.makeText(getBaseContext(), nome + " " + apelido + " " + telefone, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+}
